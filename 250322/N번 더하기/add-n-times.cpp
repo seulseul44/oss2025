12ce185fd9e27a254eb256ff7b36a52fffaf39d6
seulseul44 <kysgo03234665@gmail.com>
2025-03-22 19:42:47 +0900

    Title: N번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250322/N번 더하기/add-n-times.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,i,N=0;
+
+    cin>>A>>N;
+
+    for(i=0;i<N;i++){
+        A=A+N;
+        cout<<A<<"\n";
+    }
+    return 0;
+}

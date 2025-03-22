b0853e57f190af92175b2de6f7ef71d52a26ebb5
seulseul44 <kysgo03234665@gmail.com>
2025-03-22 19:08:55 +0900

    Title: 문자열 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250322/문자열 출력하기/print-string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, i = 0;
+
+    cin >> N;
+
+    for (i = 0; i < N; i++) {  // 조건 수정
+        cout << "LeebrosCode" << endl;  // 개행 추가
+    }
+
+    return 0;
+}

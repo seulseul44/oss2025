a4f95b04425000bd5c94c8d90e3ee8fbf4562b28
seulseul44 <kysgo03234665@gmail.com>
2025-03-23 15:11:12 +0900

    Title: M으로 계속 나누기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250323/M으로 계속 나누기/continue-dividing-by-m.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, M;
+
+    cin >> N;
+    cin >> M;
+    
+    while(N>0){
+        cout<<N<<"\n";
+        N=N/M;
+       
+    }
+    
+    return 0;
+}

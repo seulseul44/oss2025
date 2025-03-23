7b8325de419222782e4806f3e22398ad552d4428
seulseul44 <kysgo03234665@gmail.com>
2025-03-23 23:31:04 +0900

    Title: 줄 세우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250323/줄 세우기/line-up-students.cpp
+++ 250323/줄 세우기/line-up-students.cpp
@@ -29,7 +29,7 @@ int main() {
     });
 
     for (int i = 0; i < N; i++) {
-        cout << students[i].number << " " << students[i].height << " " << students[i].weight << endl;
+        cout << students[i].height << " " << students[i].weight << " " << students[i].number << endl;
     }
 
     return 0;

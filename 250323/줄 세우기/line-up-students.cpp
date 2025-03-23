c5e1ca4701c6cee61a6227cd0425a781f7392f10
seulseul44 <kysgo03234665@gmail.com>
2025-03-23 23:29:11 +0900

    Title: 줄 세우기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250323/줄 세우기/line-up-students.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int h[1000];
+int w[1000];
+
+struct Student {
+    int height;
+    int weight;
+    int number;
+};
+
+int main() {
+    cin >> N;
+    Student students[1000];
+
+    for (int i = 0; i < N; i++) {
+        cin >> students[i].height >> students[i].weight;
+        students[i].number = i + 1;
+    }
+
+    sort(students, students + N, [](const Student &a, const Student &b) {
+        if (a.height != b.height) return a.height > b.height;
+        if (a.weight != b.weight) return a.weight > b.weight;
+        return a.number < b.number;
+    });
+
+    for (int i = 0; i < N; i++) {
+        cout << students[i].number << " " << students[i].height << " " << students[i].weight << endl;
+    }
+
+    return 0;
+}

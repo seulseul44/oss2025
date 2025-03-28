a7e6df8351d3b5ed4ba64c1b3c62ed993f959f5c
seulseul44 <kysgo03234665@gmail.com>
2025-03-28 21:22:15 +0900

    Title: 별표 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250328/별표 출력하기 2/print-star-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+
+    for(int i=0;i<N;i++){
+        for(int j=N;j>i;j--){
+            cout<<"*"<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}

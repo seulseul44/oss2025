55c2f0fda984a23878adf3e7098b89a514e69a7a
seulseul44 <kysgo03234665@gmail.com>
2025-04-08 22:57:02 +0900

    Title: 5의 배수의 개수는?, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250408/5의 배수의 개수는?/the-number-of-multiples-of-5.cpp
+++ 250408/5의 배수의 개수는?/the-number-of-multiples-of-5.cpp
@@ -6,14 +6,14 @@ int main() {
     int cnt=0;
 
     for(int i=0;i<4;i++){
-        for(int j=0;<j$;j++){
+        for(int j=0;j<4;j++){
             cin>>arr[i][j];
         }
     }
      for(int i=0;i<4;i++){
-        for(int j=0;<j$;j++){
+        for(int j=0;j<4;j++){
             if(arr[i][j]%5==0){
-                cnt+=1
+                cnt+=1;
             }
             else{
                 continue;

da1de16599f80ec457ad35508c242409d5a9a926
seulseul44 <kysgo03234665@gmail.com>
2025-04-10 11:56:33 +0900

    Title: 다양하게 출력, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250410/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>  // 여기 추가!
+using namespace std;
+
+int main() {
+    cout << "Total days in Year\n" << 365 << "\n";
+    cout << "Circumference rate\n" << fixed << setprecision(10) << 3.1415926535;
+
+    return 0;
+}

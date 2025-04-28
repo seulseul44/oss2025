ee6e694fa082f80fb90136f34c078d339670e4c2
seulseul44 <kysgo03234665@gmail.com>
2025-04-29 00:33:51 +0900

    Title: 삼항연산자, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250429/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int S;
+    cin>>S;
+
+    if(S==100){
+        cout<<"pass";
+    }
+    else{
+        cout<<"failure";
+    }
+    return 0;
+}

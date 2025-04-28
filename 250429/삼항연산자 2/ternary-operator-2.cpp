b256d3070c1dd535e5e5f9e784dc63014754eaf8
seulseul44 <kysgo03234665@gmail.com>
2025-04-29 00:35:19 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250429/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+
+    if(a==1){
+        cout<<"t";
+    }
+    else{
+        cout<<"f";
+    }
+    return 0;
+}

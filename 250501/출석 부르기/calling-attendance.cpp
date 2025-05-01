4046be57d2178541485a4de9ea4daa806bb68548
seulseul44 <kysgo03234665@gmail.com>
2025-05-01 19:47:05 +0900

    Title: 출석 부르기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250501/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A;
+    cin>>A;
+
+    if(A==1){
+        cout<<"John";
+    }
+    else if(A==2){
+        cout<<"Tom";
+    }
+    else if(A==3){
+        cout<<"Paul";
+    }
+    else{
+        cout<<"Vacancy";
+    }
+    return 0;
+}

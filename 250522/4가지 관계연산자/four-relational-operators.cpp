2eef614dbd03d136854344e99150370a7830f991
seulseul44 <kysgo03234665@gmail.com>
2025-05-22 08:20:08 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250522/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B;
+    cin>>A>>B;
+
+    if (A>=B){
+        cout<<"1"<<"\n";
+    }
+    else{
+        cout<<"0"<<"\n";
+    }
+
+    if (A>B){
+        cout<<"1"<<"\n";
+    }
+    else{
+        cout<<"0"<<"\n";
+    }
+    if (A<=B){
+        cout<<"1"<<"\n";
+    }
+    else{
+        cout<<"0"<<"\n";
+    }
+    if (A<B){
+        cout<<"1"<<"\n";
+    }
+    else{
+        cout<<"0"<<"\n";
+    }
+
+    return 0;
+}

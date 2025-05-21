0ab620e80fb312d0d3ef33070a2c48ed8ccd80ca
seulseul44 <kysgo03234665@gmail.com>
2025-05-22 08:23:35 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250522/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A;
+    int B,C,D,E;
+    cin>>A>>B>>C>>D>>E;
+
+    if(A>B){
+        cout<<"1"<<"\n";
+    }
+    else {
+        cout<<"0"<<"\n";
+    }
+    if(A>C){
+        cout<<"1"<<"\n";
+    }
+    else {
+        cout<<"0"<<"\n";
+    }
+    if(A>D){
+        cout<<"1"<<"\n";
+    }
+    else {
+        cout<<"0"<<"\n";
+    }
+    if(A>E){
+        cout<<"1"<<"\n";
+    }
+    else {
+        cout<<"0"<<"\n";
+    }
+    return 0;
+}

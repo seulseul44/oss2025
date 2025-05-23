09dbb70c09f9d0cfc75f937bf591a45d7482d23c
seulseul44 <kysgo03234665@gmail.com>
2025-05-23 14:18:16 +0900

    Title: 범위 안의 수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250523/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+
+    if(a>=10&&a<=20){
+        cout<<"yes";
+    }
+    else{
+        cout<<"no";
+    }
+    return 0;
+}

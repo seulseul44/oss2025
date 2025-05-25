cf6ca2c19cc1c6195f1ce8d05f952c9a067861d3
seulseul44 <kysgo03234665@gmail.com>
2025-05-25 19:40:28 +0900

    Title: 범위 밖의 수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250525/범위 밖의 수/number-out-of-range.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a;
+    cin>>a;
+
+    if(a>20||a<10){
+        cout<<"yes";
+    }
+    else{
+        cout<<"no";
+    }
+    return 0;
+}

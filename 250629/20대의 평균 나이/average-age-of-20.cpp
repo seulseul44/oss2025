6a503124fa8f124825cdefc5ec0432bb90967cf4
seulseul44 <kysgo03234665@gmail.com>
2025-06-29 22:55:25 +0900

    Title: 20대의 평균 나이, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250629/20대의 평균 나이/average-age-of-20.cpp
+++ 250629/20대의 평균 나이/average-age-of-20.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>  // setprecision을 위해 필요
 using namespace std;
 
 int main() {
@@ -18,9 +19,10 @@ int main() {
     }
 
     if (cnt > 0) {
-        cout << (float)sum / cnt;
+        // 소수점 둘째 자리까지 고정 출력
+        cout << fixed << setprecision(2) << (float)sum / cnt;
     } else {
-        cout << "0.0";  // 예외 처리: 입력된 20대가 하나도 없는 경우
+        cout << "0.00";  // 예외 처리: 입력된 20대가 하나도 없는 경우
     }
 
     return 0;

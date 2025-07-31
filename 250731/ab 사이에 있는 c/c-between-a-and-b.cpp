999c9d9b54664626fe63069add716515a467a370
seulseul44 <kysgo03234665@gmail.com>
2025-07-31 18:40:11 +0900

    Title: ab 사이에 있는 c, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250731/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+
+    cin>>a>>b>>c;
+    
+
+    bool sat = false;
+
+    for(int i=a;i<=b;i++){
+        if(i%c==0){
+            bool sat=true;
+        }
+    }
+
+    if(sat==true){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}

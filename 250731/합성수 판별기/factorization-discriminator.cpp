72f0337e8849dc29cc538520987632e7bbf8dcc7
seulseul44 <kysgo03234665@gmail.com>
2025-07-31 18:54:10 +0900

    Title: 합성수 판별기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250731/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+    bool sat=false;
+
+    for(int i=2;i<=(N-1);i++){
+        if(N%i==0){
+            sat=true;
+        }
+    }
+    if(sat==true){
+        cout<<"C";
+    }
+    else{
+        cout<<"N";
+    }
+    return 0;
+}

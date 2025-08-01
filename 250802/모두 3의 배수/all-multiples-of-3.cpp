de42d6c770d02b92aafaf664f56b08ed1541bdf6
seulseul44 <kysgo03234665@gmail.com>
2025-08-02 01:19:28 +0900

    Title: 모두 3의 배수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250802/모두 3의 배수/all-multiples-of-3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.\
+
+    
+
+    bool sat=false;
+    int arr[5];
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+        }
+
+    for(int i=0;i<5;i++){
+        if(arr[i]%3!
+        =0){
+            sat=true;
+        }
+    }
+    if(sat==false){
+        cout<<1;
+    }
+    else{
+        cout<<0;
+    }
+
+    return 0;
+}
